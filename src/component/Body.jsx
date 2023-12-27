1aa250a344f814892e623eb2053185ed1c1213a4
mathi <sinoir.mathieu@gmail.com>
2023-12-27 15:02:38 +0100

    second

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -8,6 +8,14 @@ function Body() {
                     <section className='BodyTitre'>
                         <h1>LET ME INTRODUCE MYSELF</h1>
                         <h1>I'M JUNIOR WEB DEVELOPEUR</h1>
+                        <h2>You can check my profil </h2>
+                        <section className='SocialProfil'>
+                            <ul>
+                                <li className='LinkedingProfil'><img className='GitHubImg' src='src/assets/github.png' alt='icone github'></img></li>
+                                <li className='GuitHubProfil'><img className='LinkdinImg' src='src/assets/linkedin.png' alt='icone linkdin'></img></li>
+
+                            </ul>
+                        </section>
 
                     </section>
                 </section>
