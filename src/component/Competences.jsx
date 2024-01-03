5cdc21bb9f63f3ba36a98b4dbb92f09774d4afad
mathi <sinoir.mathieu@gmail.com>
2024-01-03 09:11:46 +0100

    ajout competences

--- src/component/Competences.jsx
+++ src/component/Competences.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
 export default function Competences() {
     return (
-        <div>Competences</div>
+
+        <section className="ComptencesSection">
+            <h1>Competences</h1>
+            <section className="CompetencesBox">
+                <div className='comptencesIcons'>
+                    <img src="src/assets/html-5.png" alt="HTML Icone" />
+                    <img src="src/assets/css-3.png" alt="CSS Icone" />
+                    <img src="src/assets/js.png" alt="Javscxript Icone" />
+                    <img src="src/assets/nodejs.png" alt="Node.js Icone" />
+                    <img src="src/assets/react.png" alt="React Icone" />
+                </div>
+            </section>
+        </section >
     )
 }
